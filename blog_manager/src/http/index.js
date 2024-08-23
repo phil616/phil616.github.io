71c0116838fe7bcf7091b922ce4d9be41535d0d5
phil616 <Phil616@163.com>
2024-08-23 15:12:59 +0800

    update bm's apiurl

--- blog_manager/src/http/index.js
+++ blog_manager/src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 创建axios实例
 const service = axios.create({
-  baseURL: process.env.VUE_APP_API 
+  baseURL: process.env.VUE_APP_API + "/api"
 });
 service.baseURL = process.env.VUE_APP_API;
 // 请求拦截器
